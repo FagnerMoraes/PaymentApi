c658010dc212a607b62346bc476a420bcf11e559
FagnerMoraes <fagm.carvalho@gmail.com>
2022-10-21 16:33:11 -0300

    Adicionar arquivos de projeto.

---
+++ PaymentApi.Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentApi.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
---
+++ PaymentApi.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentApi.Domain.Entities
+{
+    public class Product
+    {
+        public Product( string title, decimal price)
+        {
+            Title = title;
+            Price = price;
+        }
+        public Product(int id, string title, decimal price)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+        }
+
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public decimal Price { get; private set; }
+    }
+}
